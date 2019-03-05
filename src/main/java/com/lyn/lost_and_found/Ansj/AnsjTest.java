2c98f679c6fe24a352cdc3520aa619ef3cb05538
lyn <851059946@qq.com>
2019-03-05 22:53:04 +0800

    新增ansj词法分析

--- src/main/java/com/lyn/lost_and_found/Ansj/AnsjTest.java
+++ src/main/java/com/lyn/lost_and_found/Ansj/AnsjTest.java
@@ -1,14 +1,20 @@
 package com.lyn.lost_and_found.Ansj;
 
+import com.sun.tools.jdeps.Analyzer;
+import org.ansj.app.keyword.KeyWordComputer;
+import org.ansj.app.keyword.Keyword;
 import org.ansj.domain.Result;
 import org.ansj.domain.Term;
+import org.ansj.library.AmbiguityLibrary;
+import org.ansj.library.DicLibrary;
+import org.ansj.library.StopLibrary;
+import org.ansj.library.SynonymsLibrary;
+import org.ansj.recognition.impl.StopRecognition;
 import org.ansj.splitWord.analysis.NlpAnalysis;
 import org.ansj.splitWord.analysis.ToAnalysis;
 
 import java.io.*;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 使用教程参考： https://blog.csdn.net/bitcarmanlee/article/details/53607776
@@ -25,11 +31,30 @@ public class AnsjTest {
 //                "长期使用安全健康的比同龄人显小五到十岁 28岁的妹子看看你们的鱼尾纹" ;
 //        System.out.println(ToAnalysis.parse(str));
         //3
-        StringBuffer fileContent = filterStopWordsTest();
-        Result result = NlpAnalysis.parse(fileContent.toString());
-        System.out.println("--------中文分词-------");
-        System.out.println(result);
-        System.out.println("--------去停用词--------");
+//        StringBuffer fileContent = filterStopWordsTest();
+//        Result result = NlpAnalysis.parse(fileContent.toString());
+//        System.out.println("--------中文分词-------");
+//        System.out.println(result);
+//        System.out.println("--------去停用词--------");
+//        StopRecognition stopFilter = new StopRecognition();
+       //4
+        getKeywords();
+//
+//        {
+//            Map<String, String> args = new HashMap<String, String>();
+//            args.put("type", AnsjAnalyzer.TYPE.nlp_ansj.name());
+//            args.put(StopLibrary.DEFAULT, "停用词典KEY");
+//            args.put(DicLibrary.DEFAULT, "自定义词典KEY");
+//            args.put(SynonymsLibrary.DEFAULT, "同义词典KEY");
+//            args.put(AmbiguityLibrary.DEFAULT, "歧义词典KEY");
+//            args.put("isNameRecognition", "是否开启人名识别, 默认true");
+//            args.put("isNumRecognition", "是否开启数字识别, 默认true");
+//            args.put("isQuantifierRecognition", "是否开启量词识别, 默认true");
+//            args.put("isRealName", "是否保留原字符, 默认false");
+//            Analyzer analyzer = new AnsjAnalyzer(args);
+//            IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+//
+//        }
 //        FilterModifWord
     }
 
@@ -130,5 +155,14 @@ public class AnsjTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+    public static void getKeywords(){
+        KeyWordComputer kwc = new KeyWordComputer(5);
+        String title = "维基解密否认斯诺登接受委内瑞拉庇护";
+        String content = "有俄罗斯国会议员，9号在社交网站推特表示，美国中情局前雇员斯诺登，已经接受委内瑞拉的庇护，不过推文在发布几分钟后随即删除。俄罗斯当局拒绝发表评论，而一直协助斯诺登的维基解密否认他将投靠委内瑞拉。　　俄罗斯国会国际事务委员会主席普什科夫，在个人推特率先披露斯诺登已接受委内瑞拉的庇护建议，令外界以为斯诺登的动向终于有新进展。　　不过推文在几分钟内旋即被删除，普什科夫澄清他是看到俄罗斯国营电视台的新闻才这样说，而电视台已经作出否认，称普什科夫是误解了新闻内容。　　委内瑞拉驻莫斯科大使馆、俄罗斯总统府发言人、以及外交部都拒绝发表评论。而维基解密就否认斯诺登已正式接受委内瑞拉的庇护，说会在适当时间公布有关决定。　　斯诺登相信目前还在莫斯科谢列梅捷沃机场，已滞留两个多星期。他早前向约20个国家提交庇护申请，委内瑞拉、尼加拉瓜和玻利维亚，先后表示答应，不过斯诺登还没作出决定。　　而另一场外交风波，玻利维亚总统莫拉莱斯的专机上星期被欧洲多国以怀疑斯诺登在机上为由拒绝过境事件，涉事国家之一的西班牙突然转口风，外长马加略]号表示愿意就任何误解致歉，但强调当时当局没有关闭领空或不许专机降落。";
+        Collection<Keyword> result = kwc.computeArticleTfidf(title, content);
+        System.out.println(result);
     }
 }
